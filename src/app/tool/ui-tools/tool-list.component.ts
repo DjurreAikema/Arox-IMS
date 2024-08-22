56cbd9b9cb4b97ce0a3329238f9a4716de482bae
d.aikema <djurreaikema@gmail.com>
2024-08-22 16:10:52 +0200

    Tool list component

--- src/app/tool/ui-tools/tool-list.component.ts
+++ src/app/tool/ui-tools/tool-list.component.ts
@@ -1,16 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, input, output} from '@angular/core';
+import {Tool, RemoveTool} from "../../shared/interfaces";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-tool-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   template: `
-    <p>
-      tool-list works!
-    </p>
+    <ul>
+      @for (tool of tools(); track tool.id) {
+        <li>
+
+          <a routerLink="/tool/{{tool.id}}">
+            {{ tool.name }}
+          </a>
+
+          <div>
+            <button class="button-success" (click)="edit.emit(tool)">Edit</button>
+            <button class="button-danger" (click)="delete.emit(tool.id)">Delete</button>
+          </div>
+
+        </li>
+      } @empty {
+        <p>No tools found, click the add button to create your first tool.</p>
+      }
+    </ul>
   `,
-  styles: ``
+  styles: [
+    `
+      ul {
+        padding: 0;
+        margin: 0;
+      }
+
+      li {
+        display: flex;
+        justify-content: space-between;
+
+        font-size: 1.2em;
+        background: var(--color-light);
+        list-style-type: none;
+        margin-bottom: 1rem;
+        padding: 1rem;
+
+        button {
+          margin-left: 1rem;
+        }
+      }
+    `,
+  ]
 })
+// responsibility: Dumb component that displays a list of tools
 export class ToolListComponent {
+
+  // --- Inputs
+  tools = input.required<Tool[]>();
+
+  // --- Outputs
+  edit = output<Tool>();
+  delete = output<RemoveTool>();
 
 }
