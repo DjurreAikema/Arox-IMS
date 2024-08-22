09638724a1f9498bf7fdf4db14c9a88a4ecedb73
d.aikema <djurreaikema@gmail.com>
2024-08-22 16:06:21 +0200

    Tools scaffolding

---
+++ src/app/tool/tool-details.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-details',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      tool-details works!
+    </p>
+  `,
+  styles: ``
+})
+export class ToolDetailsComponent {
+
+}
---
+++ src/app/tool/ui-tools/tool-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-list',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      tool-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class ToolListComponent {
+
+}
