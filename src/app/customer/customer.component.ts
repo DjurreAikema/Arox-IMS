89907f782b1fb7f93e6a7de3e27c5fe9462c4319
d.aikema <djurreaikema@gmail.com>
2024-08-22 11:53:13 +0200

    Customer component base

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, effect, inject, signal} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+import {CustomerService} from "./data-access/customer.service";
+import {Customer} from "../shared/interfaces";
 
 @Component({
   selector: 'app-customer',
@@ -11,6 +14,31 @@ import {Component} from '@angular/core';
   `,
   styles: ``
 })
+// Responsibility: Smart component in charge of all customer interactions
 export default class CustomerComponent {
 
+  // --- Dependencies
+  public fb: FormBuilder = inject(FormBuilder);
+  public customerService: CustomerService = inject(CustomerService);
+
+  // Track the customer that is currently being edited
+  public customerBeingEdited = signal<Partial<Customer> | null>(null);
+
+  // Form for creating/editing customers
+  public customerForm = this.fb.nonNullable.group({
+    name: [''],
+  });
+
+  constructor() {
+    // Reset `customerForm` when `customerBeingEdited()` is null
+    effect((): void => {
+      const customer: Partial<Customer> | null = this.customerBeingEdited();
+      if (!customer) this.customerForm.reset(); // Imperative code
+      else {
+        this.customerForm.patchValue({
+          name: customer.name
+        });
+      }
+    });
+  }
 }
