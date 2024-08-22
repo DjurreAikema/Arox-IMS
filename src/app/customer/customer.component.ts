cdb7cc97b396d898a45f1121f388d439e21db6a3
d.aikema <djurreaikema@gmail.com>
2024-08-22 10:56:04 +0200

    Scaffolding

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-customer',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      customer works!
+    </p>
+  `,
+  styles: ``
+})
+export default class CustomerComponent {
+
+}
