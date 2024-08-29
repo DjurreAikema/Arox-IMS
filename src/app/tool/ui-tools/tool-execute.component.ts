bf9df7cfe837b6b88e10aeeb0c378b5ccf0587ae
d.aikema <djurreaikema@gmail.com>
2024-08-29 14:34:23 +0200

    Tool execute page

---
+++ src/app/tool/ui-tools/tool-execute.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-execute',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      tool-execute works!
+    </p>
+  `,
+  styles: ``
+})
+export class ToolExecuteComponent {
+
+}
