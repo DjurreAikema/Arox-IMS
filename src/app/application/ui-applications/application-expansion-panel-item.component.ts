e9d71ed2b0a6fd66fafd72ddd0fc834d925d39ad
d.aikema <djurreaikema@gmail.com>
2024-09-05 10:34:17 +0200

    Home page

--- src/app/application/ui-applications/application-expansion-panel-item.component.ts
+++ src/app/application/ui-applications/application-expansion-panel-item.component.ts
@@ -6,15 +6,52 @@ import {Application} from "../../shared/interfaces";
   standalone: true,
   imports: [],
   template: `
-    <p>
-      {{ application().name }}
-    </p>
+    <div class="expansion-panel-item" (click)="clickTest()">
+      <p>
+        {{ application().name }}
+      </p>
+
+      <div class="panel-buttons">
+        <button class="button-success small-button" (click)="onButtonClick($event)">
+          <i class="fa-solid fa-pen"></i>
+        </button>
+      </div>
+    </div>
   `,
-  styles: ``
+  styles: [`
+    .expansion-panel-item {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-between;
+      align-items: center;
+
+      padding: 1rem;
+      border-bottom: 1px solid var(--color-secondary);
+
+      p {
+        margin: 0;
+      }
+    }
+
+    .expansion-panel-item:hover {
+      background-color: lightyellow;
+      cursor: pointer;
+    }
+  `]
 })
 export class ApplicationExpansionPanelItemComponent {
 
   // --- Inputs
   application = input.required<Application>();
+
+  // --- Functions
+  protected clickTest() {
+    console.log("clicked")
+  }
+
+  protected onButtonClick(event: MouseEvent) {
+    event.stopPropagation();
+    console.log("button clicked");
+  }
 
 }
