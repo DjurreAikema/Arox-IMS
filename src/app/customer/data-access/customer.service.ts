91a049265ca8684192a2225de7f74f48d84a9125
d.aikema <djurreaikema@gmail.com>
2024-10-04 11:27:28 +0200

    Switch to using API - Customers

--- src/app/customer/data-access/customer.service.ts
+++ src/app/customer/data-access/customer.service.ts
@@ -1,7 +1,7 @@
-import {computed, effect, inject, Injectable, Signal, signal, WritableSignal} from '@angular/core';
+import {computed, inject, Injectable, Signal, signal, WritableSignal} from '@angular/core';
 import {AddCustomer, Customer, EditCustomer, RemoveCustomer} from "../../shared/interfaces";
 import {StorageService} from "../../shared/data-access/storage.service";
-import {catchError, EMPTY, map, merge, Observable, Subject} from "rxjs";
+import {catchError, exhaustMap, map, merge, Observable, of, retry, Subject} from "rxjs";
 import {connect} from "ngxtension/connect";
 import {ApplicationService} from "../../application/data-access/application.service";
 import {environment} from "../../../environments/environment";
@@ -35,96 +35,92 @@ export class CustomerService {
   public loaded: Signal<boolean> = computed(() => this.state().loaded);
 
   // --- Sources (the state gets updated when these sources get a new value, the rest of the application can use these sources to alter the data in the state)
+  public customers$: Observable<Customer[]> = this.loadCustomers().pipe(
+    retry(3),
+    catchError((err) => {
+      this.error$.next(err.message);
+      return of([]);
+    }),
+  );
+
   public add$: Subject<AddCustomer> = new Subject<AddCustomer>();
   public edit$: Subject<EditCustomer> = new Subject<EditCustomer>();
   public remove$: Subject<RemoveCustomer> = this.applicationService.customerRemoved$;
 
   private error$: Subject<string> = new Subject<string>();
-  private customersLoaded$: Observable<Customer[]> = this.storageService.loadCustomers().pipe(
-    catchError((err) => {
-      this.error$.next(err);
-      return EMPTY;
-    })
-  );
 
   // --- Reducers (reducers subscribe to the sources and update the actual state)
   constructor() {
     const nextState$ = merge(
-      // error$ reducer
-      this.error$.pipe(map((error) => ({error}))),
+      // customers$ reducer, update state when customers loaded
+      this.customers$.pipe(map((customers) => ({
+        customers: customers,
+        loaded: true
+      }))),
 
-      // customersLoaded$ reducer
-      this.customersLoaded$.pipe(
-        map((customers) => ({customers, loaded: true}))
-      )
+      // add$ reducer
+      this.add$.pipe(
+        exhaustMap((customer) =>
+          this.addNewCustomer(customer).pipe(
+            // Update the state
+            map((newCustomer) => ({
+              customers: [...this.customers(), newCustomer]
+            })),
+            catchError((error) => of({error: error.message}))
+          )
+        )
+      ),
+
+      // edit$ reducer
+      this.edit$.pipe(
+        exhaustMap((update) =>
+          this.editCustomer(update).pipe(
+            // Update the state
+            map((updatedCustomer) => ({
+              customers: this.customers().map((customer) =>
+                customer.id === updatedCustomer.id ? updatedCustomer : customer
+              )
+            })),
+            catchError((error) => of({error: error.message}))
+          )
+        )
+      ),
+
+      // remove$ reducer
+      this.remove$.pipe(
+        exhaustMap((id) =>
+          this.removeCustomer(id).pipe(
+            // Update the state
+            map(() => ({
+              customers: this.customers().filter((customer) => customer.id !== id)
+            })),
+            catchError((error) => of({error: error.message}))
+          )
+        )
+      ),
+
+      // Update error state
+      this.error$.pipe(map((error) => ({error}))),
     );
 
     connect(this.state)
-      .with(nextState$)
-
-      // add$ reducer
-      .with(this.add$, (state, customer) => ({
-        customers: [...state.customers, this.addIdToCustomer(customer)],
-      }))
-
-      // remove$ reducer
-      .with(this.remove$, (state, id) => ({
-        customers: state.customers.filter((customer) => customer.id !== id),
-      }))
-
-      // edit$ reducer
-      .with(this.edit$, (state, update) => ({
-        customers: state.customers.map((customer) =>
-          customer.id === update.id
-            ? {...customer, name: update.data.name}
-            : customer
-        ),
-      }));
-
-    // --- Effects (effects are used to chain certain actions to state updates)
-    effect((): void => {
-      // this effect saves the customers to local storage every time the state changes
-      if (this.loaded()) {
-        this.saveCustomers(this.customers());
-      }
-    });
+      .with(nextState$);
   }
 
   // --- Functions (these functions are used exclusively inside this state)
-  private addNewCustomer(customer: AddCustomer) {
-    return this.http.post<Customer>(`${environment.apiUrl}/customers`, customer).pipe(
-      catchError((err) => {
-        this.error$.next(err);
-        return EMPTY;
-      })
-    );
+  private loadCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(`${environment.apiUrl}/customers`);
   }
 
-  private saveCustomers(customers: Customer[]): void {
-    // If DemoMode is enabled, use local storage
-    if (environment.demoMode) {
-      this.storageService.saveCustomers(customers);
-      return;
-    }
-
-    // Else use the API
-    this.http.post(`${environment.apiUrl}/customers/addOrUpdate`, customers)
-      .pipe(
-        catchError((err) => {
-          // TODO: Handle error
-          return EMPTY;
-        })
-      ).subscribe();
+  private addNewCustomer(customer: AddCustomer): Observable<Customer> {
+    return this.http.post<Customer>(`${environment.apiUrl}/customers`, customer);
   }
 
-  private addIdToCustomer(customer: AddCustomer) {
-    return {
-      ...customer,
-      id: this.generateId()
-    };
+  private editCustomer(update: EditCustomer): Observable<Customer> {
+    return this.http.put<Customer>(`${environment.apiUrl}/customers/${update.id}`, update.data);
   }
 
-  private generateId() {
-    return this.customers().reduce((maxId, customer) => Math.max(maxId, customer.id), 0) + 1;
+  private removeCustomer(id: number): Observable<void> {
+    return this.http.delete<void>(`${environment.apiUrl}/customers/${id}`);
   }
 }
