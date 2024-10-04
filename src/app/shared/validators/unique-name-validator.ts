4d6c526593a928ef66561ea3b89021d2e77c2963
d.aikema <djurreaikema@gmail.com>
2024-10-04 15:16:16 +0200

    Switch to using API - Tool Outputs

--- src/app/shared/validators/unique-name-validator.ts
+++ src/app/shared/validators/unique-name-validator.ts
@@ -4,7 +4,9 @@ export function uniqueNameValidator(existingNames?: string[]): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const name = control.value;
     if (!existingNames) return null;
-    if (existingNames.includes(name)) {
+
+    const filteredNames = existingNames.filter(existingName => existingName !== name);
+    if (filteredNames.includes(name)) {
       return {uniqueName: {value: name}};
     }
     return null;
